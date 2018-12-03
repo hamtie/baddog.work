610c834c6892ca067ded6054f2707f1a2eb24d45
Hannah Fouasnon <hanelifou@gmail.com>
2018-12-03 03:27:47 -0800

    Change landing page.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <BadDog msg="Protect yourself"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BadDog from './components/BadDog.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    BadDog
   }
 }
 </script>
